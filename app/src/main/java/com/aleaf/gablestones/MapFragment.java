98a4abde86239300fdad251f21c85aca4a712590
AngelikaDw <1ngelika>
2017-06-26 14:35:35 +0200

    Map implemented FAB for MAP fragment, but not yet clickable

--- app/src/main/java/com/aleaf/gablestones/MapFragment.java
+++ app/src/main/java/com/aleaf/gablestones/MapFragment.java
@@ -1,36 +1,102 @@
 package com.aleaf.gablestones;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-/**
- * Created by User on 2/28/2017.
- */
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment {
+
+
+
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
     private static final String TAG = "MapFragment";
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
+    FloatingActionButton mFab3;
 
-    private Button btnTEST;
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.map_fragment,container,false);
-        btnTEST = (Button) view.findViewById(R.id.btnTEST3);
 
-        btnTEST.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 3",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        return view;
+    public MapFragment() {
+        //Required empty public constructor
     }
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mView =inflater.inflate(R.layout.map_fragment,container,false);
+
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView = (MapView) mView.findViewById(R.id.map);
+        if (mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+
+        mGoogleMap = googleMap;
+        LatLng Amsterdam = new LatLng(52.372438, 4.900327);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions()
+                .position(Amsterdam)
+                .title("Center of Amsterdam")
+                .snippet("Maybe one day, I will be there with my family"));
+        CameraPosition Liberty = CameraPosition.builder()
+                .target(Amsterdam)
+                .zoom(13)
+                .bearing(0)
+                .tilt(0)
+                .build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
+    }
+
+
+
+
+
+//
+//
+//    private FloatingActionButton mapFab;
+
+
 }
