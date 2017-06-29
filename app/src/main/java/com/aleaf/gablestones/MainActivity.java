d7bfe0c840365601c8239c50678cefed64ff8bbc
AngelikaDw <1ngelika>
2017-06-29 12:10:00 +0200

    Current Location of User is shown automatically

--- app/src/main/java/com/aleaf/gablestones/MainActivity.java
+++ app/src/main/java/com/aleaf/gablestones/MainActivity.java
@@ -8,8 +8,7 @@ package com.aleaf.gablestones;
     import android.util.Log;
     import android.view.Menu;
     import android.view.MenuItem;
-    import android.view.View;
-    import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +18,6 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
 
-    private FloatingActionButton fab1;
-    private FloatingActionButton fab2;
-    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,67 +37,6 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-        // FAB
-        //fab1 = (FloatingActionButton) findViewById(R.id.fab1);
-        //fab2 = (FloatingActionButton) findViewById(R.id.fab2);
-
-        //showRightFab(mViewPager.getCurrentItem());
-
-        //add a listener to the ViewPager to call the showRightFab(...) function each time the
-        // selected tab changes.
-
-//        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-//            @Override
-//            public void onPageSelected(int position) {
-//                showRightFab(position);
-//            }
-//            @Override
-//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
-//            @Override
-//            public void onPageScrollStateChanged(int state) {}
-//        });
-
-
-//        TabLayout.OnTabSelectedListener onTabSelectedListener = new TabLayout.OnTabSelectedListener() {
-//            @Override
-//            public void onTabSelected(TabLayout.Tab tab) {
-//                animateFab(tab.getPosition());
-//            }
-//
-//            @Override
-//            public void onTabUnselected(TabLayout.Tab tab) {
-//
-//            }
-//
-//            @Override
-//            public void onTabReselected(TabLayout.Tab tab) {
-//
-//            }
-//        };
-
-//        ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
-//            @Override
-//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//
-//            }
-//
-//            @Override
-//            public void onPageSelected(int position) {
-//                animateFab(position);
-//            }
-//
-//            @Override
-//            public void onPageScrollStateChanged(int state) {
-//
-//            }
-//        };
-
-//        fab1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(mViewPager.getContext(), "Testing FAB Intro", Toast.LENGTH_SHORT).show();
-//            }
-//        });
     }
 
     private void setupViewPager(ViewPager viewPager) {
@@ -134,48 +69,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-//    // Function to show the right FAB for the given tab. Default case, just call hide method on tabs.
-//    public void showRightFab(int tab) {
-//        switch (tab) {
-//            case 0:
-//                fab2.hide(new FloatingActionButton.OnVisibilityChangedListener() {
-//                    @Override
-//                    public void onHidden(FloatingActionButton fab) {
-//                        fab1.show();
-//                    }
-//                });
-//                break;
-//            case 1:
-//                fab1.hide(new FloatingActionButton.OnVisibilityChangedListener(){
-//                    @Override
-//                    public void onHidden(FloatingActionButton fab) {
-//                        fab2.show();
-//                    }
-//                });
-//                break;
-//            default:
-//                fab1.hide();
-//                fab2.hide();
-//                break;
-//        }
-//    }
-//    private void animateFab(int position) {
-//        switch (position) {
-//            case 0:
-//                fab1.show();
-//                fab2.hide();
-//                break;
-//            case 1:
-//                fab2.show();
-//                fab1.hide();
-//                break;
-//
-//            default:
-//                fab1.show();
-//                fab2.hide();
-//                break;
-//        }
-//    }
+
 
 
 }
--- app/src/main/java/com/aleaf/gablestones/MapFragment.java
+++ app/src/main/java/com/aleaf/gablestones/MapFragment.java
@@ -1,5 +1,8 @@
 package com.aleaf.gablestones;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -33,35 +36,54 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.FusedLocationProviderClient;
+
+import java.util.ArrayList;
 
 
+public class MapFragment extends Fragment implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        com.google.android.gms.location.LocationListener {
 
-
-
-public class MapFragment extends Fragment implements OnMapReadyCallback {
     private static final String TAG = "MapFragment";
+    private static final int MY_PERMISSIONS_REQUEST = 1;
+    private final String LOG_TAG = AppCompatActivity.class.getSimpleName();
+    boolean mapReady = false;
     GoogleMap mGoogleMap;
     MapView mMapView;
     View mView;
-    //private FloatingActionButton fab3;
+    Marker marker;
+    TextView txtOutput;
+    ArrayList<String[]> coordinates = new ArrayList<>();
 
-
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     public MapFragment() {
         //Required empty public constructor
     }
 
+
     @Override
-    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mView =inflater.inflate(R.layout.map_fragment,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.map_fragment, container, false);
         FloatingActionButton FAB3;
         FAB3 = (FloatingActionButton) mView.findViewById(R.id.fab3);
         FAB3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "TESTING FAB locate me",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "TESTING FAB locate me", Toast.LENGTH_SHORT).show();
             }
         });
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        configureLocationUpdates();
         return mView;
     }
 
@@ -87,7 +109,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         googleMap.addMarker(new MarkerOptions()
                 .position(Amsterdam)
                 .title("Center of Amsterdam")
-                .snippet("Maybe one day, I will be there with my family"));
+                .snippet("Maybe one day, I will be there with my family")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
         CameraPosition Liberty = CameraPosition.builder()
                 .target(Amsterdam)
                 .zoom(13)
@@ -97,6 +120,122 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        //This check necessary for API 23 or higher (Android 6.0)
+        int permissionCheck = ActivityCompat.checkSelfPermission(
+                getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION);
 
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) {
 
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(getActivity(), new String[]{
+                        android.Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST);
+            }
+
+        } else {
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                    mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+    private void requestLocationUpdate() {
+
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                getContext(),android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    requestLocationUpdate();
+                } else {
+                    txtOutput.setText("No permission to get the location");
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(LOG_TAG, "GoogleApiClient connection has been suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(LOG_TAG, "GoogleApiClient connection has failed");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.v("CURRENT LOCATION", location.toString());
+        LatLng currentLoc = new LatLng(location.getLatitude(), location.getLongitude());
+
+        if (marker != null) {
+            marker.remove();
+        }
+
+        marker = mGoogleMap.addMarker(new MarkerOptions()
+                .position(currentLoc)
+                .title("Here are you!")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        //Reposition the map to display current location
+        CameraPosition CURRENT = CameraPosition.builder().target(currentLoc).zoom(13).build();
+        flyTo(CURRENT);
+        //Adds Markers to the map from where_water1.txt
+        //addMarkersToMap();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        //Connect the Client
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        //Disconnect the Client
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    private void configureLocationUpdates() {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1 * 60000); //Update location every 3 minutes
+    }
+
+    private void flyTo(CameraPosition target) {
+        mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(target), 2000, null);
+    }
+
+//    public void addMarkersToMap() {
+//        for (int j = 1; j<coordinates.size(); j ++){
+//            String fountainaddress = coordinates.get(j)[2] +", "+coordinates.get(j)[3];
+//            double fountainlat = Double.parseDouble(coordinates.get(j)[0].trim());
+//            double fountainlng = Double.parseDouble(coordinates.get(j)[1].trim());
+//            LatLng fountainCoordinates = new LatLng(fountainlat, fountainlng);
+//
+//            mGoogleMap.addMarker(new MarkerOptions()
+//                    .position(fountainCoordinates)
+//                    .title(fountainaddress)
+//                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+//        }
+//    }
 }
